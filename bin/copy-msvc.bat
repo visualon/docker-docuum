3c153293ca3d2d9a82f75fa041acedf177ba5b04
Michael Kriese <michael.kriese@visualon.de>
2021-07-13 16:57:37 +0200

    feat: remove msvc workaround

--- bin/copy-msvc.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-for /f "usebackq delims=#" %%a in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do call "%%a\Common7\Tools\VsDevCmd.bat" -arch=amd64
-
-xcopy /i "%VCToolsRedistDir%onecore\x64\Microsoft.VC142.CRT" "windows/bin"
